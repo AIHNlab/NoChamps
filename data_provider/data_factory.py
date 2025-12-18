ef88fa3b6acedb897e1951876e103199fd7184ec
rafael <rafael.morand@unibe.ch>
2025-12-18 14:52:16 +0100

    add UTSD datasets

--- data_provider/data_factory.py
+++ data_provider/data_factory.py
@@ -1,5 +1,5 @@
 from data_provider.data_loader import Dataset_ETT_hour, Dataset_ETT_minute, Dataset_Custom, Dataset_M4, PSMSegLoader, \
-    MSLSegLoader, SMAPSegLoader, SMDSegLoader, SWATSegLoader, UEAloader
+    MSLSegLoader, SMAPSegLoader, SMDSegLoader, SWATSegLoader, UEAloader, UTSD_Npy
 from data_provider.uea import collate_fn
 from torch.utils.data import DataLoader
 
@@ -15,7 +15,8 @@ data_dict = {
     'SMAP': SMAPSegLoader,
     'SMD': SMDSegLoader,
     'SWAT': SWATSegLoader,
-    'UEA': UEAloader
+    'UEA': UEAloader,
+    'UTSD': UTSD_Npy,
 }
 
 
@@ -60,6 +61,23 @@ def data_provider(args, flag):
             drop_last=drop_last,
             collate_fn=lambda x: collate_fn(x, max_len=args.seq_len)
         )
+        return data_set, data_loader
+    elif args.data == 'UTSD':
+        data_set = Data(
+            root_path=args.root_path,
+            flag=flag,
+            size=[args.seq_len, args.label_len, args.pred_len],
+            data_path=args.data_path,
+            stride=args.stride,
+            split=args.split,
+        )
+        print(flag, len(data_set))
+        data_loader = DataLoader(
+            data_set,
+            batch_size=batch_size,
+            shuffle=shuffle_flag,
+            num_workers=args.num_workers,
+            drop_last=drop_last)
         return data_set, data_loader
     else:
         if args.data == 'm4':
